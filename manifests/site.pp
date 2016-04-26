bc3d1536c8fb44662215f9c963e5cb652240570d
Kris Buytaert <sdog@inuits.eu>
2016-04-26 17:09:50 +0200

    Disabling iptables

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,28 @@
 
+yumrepo { 'sonar':
+  ensure   => 'present',
+  baseurl  => 'http://downloads.sourceforge.net/project/sonar-pkg/rpm',
+  descr    => 'Sonar',
+  gpgcheck => '0',
+}
+
 
 class { 'java': }
 class { 'sonarqube':
-  version    => '5.1',
-  usepackage => true,
+  version      => '5.4-1',
+  usepackage   => true,
+  installroot  => '/opt/sonar/',
+  home         => '/opt/sonar',
+  package_name => 'sonar',
+
 }
 class {'sonarqube::runner':
   usepackage => true,
   version    => '2.4-1',
 }
+
+service { 'iptables':
+  ensure => 'stopped',
+  enable => 'false',
+}
+
